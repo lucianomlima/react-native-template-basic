3e648bd0162349a2b1d417405208442af9bebe92
Luciano Lima <beavess@gmail.com>
2018-08-24 03:32:54 -0300

    refactor: Move App.js to src/App

---
+++ src/App/__tests__/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import App from '..';
+
+test('should render properly', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
