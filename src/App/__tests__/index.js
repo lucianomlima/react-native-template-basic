ee3e01d42bcf9f85d0f9f73624af4ca69db92bc4
Luciano Lima <beavess@gmail.com>
2018-10-28 14:13:10 -0300

    refactor(App): Format files and update features list

--- src/App/__tests__/index.js
+++ src/App/__tests__/index.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import React from 'react';
 import renderer from 'react-test-renderer';
 
--- src/App/index.js
+++ src/App/index.js
@@ -1,35 +1,12 @@
 /**
  * React Native Basic App
  * https://github.com/facebook/react-native
+ *
+ * @format
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>Your App is ready and you already have:</Text>
-        <View style={styles.featuresList}>
-          <Text style={styles.feature}>{'\u2705 ESLint'}</Text>
-          <Text style={styles.feature}>{'\u2705 Custom Scripts'}</Text>
-        </View>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -57,3 +34,25 @@ const styles = StyleSheet.create({
     marginVertical: 5,
   },
 });
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu',
+});
+
+const App = (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>Welcome to React Native!</Text>
+    <Text style={styles.instructions}>Your App is ready and you already have:</Text>
+    <View style={styles.featuresList}>
+      <Text style={styles.feature}>{'\u2705 ESLint'}</Text>
+      <Text style={styles.feature}>{'\u2705 Prettier'}</Text>
+      <Text style={styles.feature}>{'\u2705 Lint Staged'}</Text>
+      <Text style={styles.feature}>{'\u2705 Custom Scripts'}</Text>
+    </View>
+    <Text style={styles.instructions}>To get started, edit App.js</Text>
+    <Text style={styles.instructions}>{instructions}</Text>
+  </View>
+);
+
+export default App;
