ca9fe6299a3927b288741f6400debf7b6519f5bd
Luciano Lima <beavess@gmail.com>
2019-06-02 01:35:56 -0300

    Update jest config

--- src/config/tests/setup.js
+++ src/config/tests/setup.js
@@ -1,7 +1,7 @@
 /** @format */
 
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from 'enzyme'; // eslint-disable-line import/no-extraneous-dependencies
+import Adapter from 'enzyme-adapter-react-16'; // eslint-disable-line import/no-extraneous-dependencies
 
 Enzyme.configure({
   adapter: new Adapter(),
